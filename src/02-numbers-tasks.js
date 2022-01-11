8d7c1df1a3a92fd7efef99b7bc620a526119c793
Tsvetinskaya-L <tsvetinskaya.lidiya@gmail.com>
2022-01-11 17:21:08 +0300

    fix: eslint

--- src/02-numbers-tasks.js
+++ src/02-numbers-tasks.js
@@ -155,7 +155,7 @@ function parseNumberFromString(value) {
  *   1,2,3   => 3.741657386773941
  */
 function getParallelepipedDiagonal(a, b, c) {
-  return Math.sqrt(Math.pow(a, 2) + Math.pow(b, 2) + Math.pow(c, 2));
+  return Math.sqrt(a ** 2 + b ** 2 + c ** 2);
 }
 
 /**
